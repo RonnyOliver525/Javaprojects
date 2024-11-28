0289e15afd8ea028daadbec214fdc7f6f2606233
Ronny Wallace <93355263+RonnyOliver525@users.noreply.github.com>
2024-11-28 13:06:41 -0300

    Delete Browser.java

--- Browser.java
+++
@@ -1,9 +0,0 @@
-package DesafioDIO;
-
-public interface Browser {
-	
-	boolean conexaoInternet = false;
-	public void exibirPagina(String url);
-    public void adicionarNovaAba(String url);
-    public void atualizarPagina();
-}
